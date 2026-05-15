8d798caad098bdb54b5e5ee96b0bddda2294c170
waes891106-collab <waes891106@gmail.com>
2026-05-15 09:28:46 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch',()=>{});
+const CACHE_NAME="abyss-fixed-v3";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
